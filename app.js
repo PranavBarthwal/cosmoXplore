cd73092867fc13c4e646981f48537898f1a43b53
Pranav Barthwal <110532770+PranavBarthwal@users.noreply.github.com>
2024-01-26 17:09:45 +0000

    mars section functional

--- app.js
+++ app.js
@@ -143,26 +143,40 @@ particlesJS("bg", {
 
   // NASA API CALL 2 : Mars Rover Photos
 function displayRover(){
-  
-
 
   fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=eISHzmGbQk7EMWv9NTlVtvzZVmeLKPUoL3uKMSPJ')
   .then((response) => {
       return response.json()
   }).then((data) => {
       console.log(data)
-      console.log()
+      console.log(data.photos[0].launch_date)
       let img_src = data.photos[0].img_src;
+      let date = data.photos[0].earth_date;
+      let roverName = data.photos[0].rover.name;
+      let camera = data.photos[0].camera.full_name;
+      let launch = data.photos[0].launch_date;
+      let land = data.photos[0].rover.landing_date;
+      let status = data.photos[0].rover.status;
+      
 
-      updateDom(img_src)
-    
+
+      updateDom(img_src, date, roverName, camera, launch, land, status)    
      
   })
   .catch((error) => console.log(error))
 
 
-  function updateDom(img_src){
+  function updateDom(img_src, date, roverName, camera, launch, land, status){
 
     document.querySelector('#roverImg').src = img_src
+    document.querySelector('.date').textContent = date
+    document.querySelector('.roverName').textContent = roverName
+    document.querySelector('.camera').textContent = camera
+    document.querySelector('.land').textContent = land
+    document.querySelector('.launch').textContent = launch
+    document.querySelector('.status').textContent = status
+
+
+
   }
 }
