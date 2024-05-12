1d1545fefc79e8f72089d823e3b96bd02818f74d
Uma Chandra Sekhara Sarma <chandu06p@gmail.com>
2024-05-12 13:19:35 +0530

    add comments

--- app.js
+++ app.js
@@ -184,6 +184,7 @@ function displayRover(){
     document.querySelector('.status').textContent = status
 }
 
+// added closeRover function 
 function closeRover(e){
   document.querySelector('.rover_container').style.display = 'flex';
   document.querySelector('.rover_display').style.display = 'none';
