79b000869ef89b82f780c557726d45a616c789db
Donald Reddy Indelu <donaldreddy2712@gmail.com>
2024-05-25 00:34:30 +0530

    update

--- src/pages/NasaProjects/NasaProjects.jsx
+++ src/pages/NasaProjects/NasaProjects.jsx
@@ -4,11 +4,19 @@ import Styles from "./NasaProjects.module.css"
 import { FaSearch } from "react-icons/fa";
 import { FaAnglesRight } from "react-icons/fa6";
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
+import { IoReloadOutline } from "react-icons/io5";
+
 
 
 function TechNews() {
 
     const [showSearch, setShowSearch] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isFetch, setIsFetch] = useState(true);
+    const [projectIds, setProjectIds] = useState([]);
+
+    const today_date = new Date();
+
 
     useEffect(() => {
         let position = window.scrollY;
@@ -18,6 +26,34 @@ function TechNews() {
             }, 200)
         }
     }, [showSearch])
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+
+    async function fetchData() {
+
+        try {
+            let temp = [];
+
+            console.log("call");
+
+            let response = await fetch("https://techport.nasa.gov/api/projects?updatedSince=2024-05-01&api_key=eISHzmGbQk7EMWv9NTlVtvzZVmeLKPUoL3uKMSPJ")
+
+            console.log(response);
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+
+    function handleLoadMore() {
+        setIsLoading((prev) => true);
+        console.log(isLoading);
+        setTimeout(() => setIsLoading(false), 5000)
+    }
 
 
     return (
@@ -48,35 +84,9 @@ function TechNews() {
                 <ProjectCard projectId="117090" title="Urban Air Mobility (UAM) Operational Fleet Noise Assessment
 " acronym="AAVP26" description="<p>In order to address the need for a documented methodology for generating noise assessments of fleet operations of UAM eVTOL aircraft concepts, NASA will deliver validated tools and tool chain documentation at TRL&#61;5, document best practices for fleet noise modeling, and demonstrate fleet noise assessments of representative UAM operations.</p> " startDate="Mar 2020" endDate="Jun 2024" lastUpdated="2024-5-23
 " status="Active" />
-                <ProjectCard projectId="117090" title="Urban Air Mobility (UAM) Operational Fleet Noise Assessment
-" acronym="AAVP26" description="<p>In order to address the need for a documented methodology for generating noise assessments of fleet operations of UAM eVTOL aircraft concepts, NASA will deliver validated tools and tool chain documentation at TRL&#61;5, document best practices for fleet noise modeling, and demonstrate fleet noise assessments of representative UAM operations.</p> " startDate="Mar 2020" endDate="Jun 2024" lastUpdated="2024-5-23
-" status="Active" />
-                <ProjectCard projectId="117090" title="Urban Air Mobility (UAM) Operational Fleet Noise Assessment
-" acronym="AAVP26" description="<p>In order to address the need for a documented methodology for generating noise assessments of fleet operations of UAM eVTOL aircraft concepts, NASA will deliver validated tools and tool chain documentation at TRL&#61;5, document best practices for fleet noise modeling, and demonstrate fleet noise assessments of representative UAM operations.</p> " startDate="Mar 2020" endDate="Jun 2024" lastUpdated="2024-5-23
-" status="Active" />
-                <ProjectCard projectId="117090" title="Urban Air Mobility (UAM) Operational Fleet Noise Assessment
-" acronym="AAVP26" description="<p>In order to address the need for a documented methodology for generating noise assessments of fleet operations of UAM eVTOL aircraft concepts, NASA will deliver validated tools and tool chain documentation at TRL&#61;5, document best practices for fleet noise modeling, and demonstrate fleet noise assessments of representative UAM operations.</p> " startDate="Mar 2020" endDate="Jun 2024" lastUpdated="2024-5-23
-" status="Active" />
-                <ProjectCard projectId="117090" title="Urban Air Mobility (UAM) Operational Fleet Noise Assessment
-" acronym="AAVP26" description="<p>In order to address the need for a documented methodology for generating noise assessments of fleet operations of UAM eVTOL aircraft concepts, NASA will deliver validated tools and tool chain documentation at TRL&#61;5, document best practices for fleet noise modeling, and demonstrate fleet noise assessments of representative UAM operations.</p> " startDate="Mar 2020" endDate="Jun 2024" lastUpdated="2024-5-23
-" status="Active" />
-                <ProjectCard projectId="117090" title="Urban Air Mobility (UAM) Operational Fleet Noise Assessment
-" acronym="AAVP26" description="<p>In order to address the need for a documented methodology for generating noise assessments of fleet operations of UAM eVTOL aircraft concepts, NASA will deliver validated tools and tool chain documentation at TRL&#61;5, document best practices for fleet noise modeling, and demonstrate fleet noise assessments of representative UAM operations.</p> " startDate="Mar 2020" endDate="Jun 2024" lastUpdated="2024-5-23
-" status="Active" />
-                <ProjectCard projectId="117090" title="Urban Air Mobility (UAM) Operational Fleet Noise Assessment
-" acronym="AAVP26" description="<p>In order to address the need for a documented methodology for generating noise assessments of fleet operations of UAM eVTOL aircraft concepts, NASA will deliver validated tools and tool chain documentation at TRL&#61;5, document best practices for fleet noise modeling, and demonstrate fleet noise assessments of representative UAM operations.</p> " startDate="Mar 2020" endDate="Jun 2024" lastUpdated="2024-5-23
-" status="Active" />
-                <ProjectCard projectId="117090" title="Urban Air Mobility (UAM) Operational Fleet Noise Assessment
-" acronym="AAVP26" description="<p>In order to address the need for a documented methodology for generating noise assessments of fleet operations of UAM eVTOL aircraft concepts, NASA will deliver validated tools and tool chain documentation at TRL&#61;5, document best practices for fleet noise modeling, and demonstrate fleet noise assessments of representative UAM operations.</p> " startDate="Mar 2020" endDate="Jun 2024" lastUpdated="2024-5-23
-" status="Active" />
-                <ProjectCard projectId="117090" title="Urban Air Mobility (UAM) Operational Fleet Noise Assessment
-" acronym="AAVP26" description="<p>In order to address the need for a documented methodology for generating noise assessments of fleet operations of UAM eVTOL aircraft concepts, NASA will deliver validated tools and tool chain documentation at TRL&#61;5, document best practices for fleet noise modeling, and demonstrate fleet noise assessments of representative UAM operations.</p> " startDate="Mar 2020" endDate="Jun 2024" lastUpdated="2024-5-23
-" status="Active" />
-                <ProjectCard projectId="117090" title="Urban Air Mobility (UAM) Operational Fleet Noise Assessment
-" acronym="AAVP26" description="<p>In order to address the need for a documented methodology for generating noise assessments of fleet operations of UAM eVTOL aircraft concepts, NASA will deliver validated tools and tool chain documentation at TRL&#61;5, document best practices for fleet noise modeling, and demonstrate fleet noise assessments of representative UAM operations.</p> " startDate="Mar 2020" endDate="Jun 2024" lastUpdated="2024-5-23
-" status="Active" />
-
             </div>
+
+            <button id={Styles['load_more']} onClick={handleLoadMore}>Load More <IoReloadOutline className={isLoading ? Styles['reload'] : ''} /></button>
 
         </div>
     )
