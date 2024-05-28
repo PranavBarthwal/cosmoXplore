5f509790dcf4d2795bf4c3e6848a17e57df07f08
Donald Reddy Indelu <donaldreddy2712@gmail.com>
2024-05-29 00:48:22 +0530

    add particles

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import Toast from "./components/Toast/Toast.jsx"
+import Background from './components/Background/Background.jsx'
 
 function App() {
   return (
     <>
+      <Background />
       <Toast />
       <Navbar />
       <Outlet />
