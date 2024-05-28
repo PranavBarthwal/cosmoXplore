56d1b8fd965ae4e471827f92ba75a642eb966fb1
Donald Reddy Indelu <donaldreddy2712@gmail.com>
2024-05-29 01:18:52 +0530

    update

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
-import tsconfig from '../../../tsparticlesconfig.json'
+import tsconfig from '../../../tsparticles.config.json'
 
 function Background() {
   const [init, setInit] = useState(false);
@@ -9,13 +9,7 @@ function Background() {
   // this should be run only once per application lifetime
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
       await loadSlim(engine);
-      //await loadBasic(engine);
     }).then(() => {
       setInit(true);
     });
