aef8588f0bf938db158b982b070d3e4f7ab8269d
Donald Reddy Indelu <donaldreddy2712@gmail.com>
2024-05-31 09:49:14 +0530

    update

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,110 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Styles from "./About.module.css"
+import vision from "../../assets/vision.svg"
+import method from "../../assets/method.svg"
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+
+
 
 function About() {
+
+    const FOUNDER_NAME = "PranavBarthwal"
+    const FOUNDER_LINKEDIN_URL = "https://www.linkedin.com/in/pranavbarthwal03/"
+    const CARDS_PER_PAGE = 10;
+    const repoName = "cosmoXplore";
+
+    const [contributors, setContributors] = useState([]);
+    const [founder, setFounder] = useState({});
+
+
+    useEffect(() => {
+        fetchContributors()
+    }, [])
+
+    async function fetchContributors() {
+        try {
+
+            const response = await fetch(`https://api.github.com/repos/${FOUNDER_NAME}/${repoName}/contributors`)
+
+            const data = await response.json();
+
+            console.log(data);
+
+            setContributors((prev) => {
+                return data.filter((user, idx) => user.login != FOUNDER_NAME);
+            })
+
+            setFounder(data.find((user) => user.login == FOUNDER_NAME));
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+
     return (
-        <div>About</div>
+        <div id={Styles.container}>
+
+            <section id={Styles.section_1}>
+                <h1>About CosmoXplore</h1>
+                <p><b>
+                    ''Making space exploration accessible and exciting for everyone, CosmoXplore is your portal to the universe's wonders.''
+                </b></p>
+            </section>
+
+            <section id={Styles.section_2}>
+                <h1>Our Vision</h1>
+                <div>
+                    <p>Explore the cosmos with CosmoXplore and stay informed about the innovations driving humanity's journey into the final frontier. Join us in discovering the beauty and mysteries of space, one photo and breakthrough at a time.As an open source organization, we are committed to transparency, collaboration, and community-driven development.</p>
+                    <img src={vision} width='400px' height='400px' />
+                </div>
+            </section>
+
+            <section id={Styles.section_3}>
+                <h1>Our Methods</h1>
+                <div>
+                    <img src={method} width='400px' height='400px' />
+                    <p>Our platform brings you stunning Astronomy Picture of the Day (APOD) images, fascinating photos of Mars, and updates on the latest technological breakthroughs at NASA. Utilizing NASA's open API, we deliver a seamless and engaging experience for space enthusiasts and curious minds alike.</p>
+                </div>
+            </section>
+
+            <section id={Styles.section_4}>
+
+                <div id={Styles.founder}>
+                    <h1>Our Founder</h1>
+                    <div>
+                        <img src={founder.avatar_url} />
+                        <h3>{founder.login}</h3>
+                        <div id={Styles.social}>
+                            <a href={founder.html_url} target='_blank'><FaGithub size={30} /></a>
+                            <a href={FOUNDER_LINKEDIN_URL} target='_blank'><FaLinkedin size={30} /></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div id={Styles.contributors}>
+                    <h2>Our Contributors</h2>
+                    <div id={Styles.cards}>
+                        {
+                            contributors.map((user) => (
+                                <div className={Styles.card}>
+                                    <img src={user.avatar_url} />
+                                    <h3>{user.login}</h3>
+                                    <div id={Styles.social}>
+                                        <a href={user.html_url} target='_blank'><FaGithub size={30} /></a>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+
+                </div>
+
+            </section>
+
+
+        </div>
     )
 }
 
