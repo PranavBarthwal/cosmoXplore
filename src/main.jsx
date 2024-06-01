aa21ec2a0e789b7415397fdf7e0b404c4b427c79
RamakrushnaBiswal <ramakrushnabunty@gmail.com>
2024-06-01 10:01:31 +0530

    404 page is added with configuration

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Home from "./pages/Home/Home.jsx"
 import NasaProjects from "./pages/NasaProjects/NasaProjects.jsx"
+import NotFoundPage from "./pages/404 Page/NotfoundPage.jsx"
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom"
 
@@ -11,6 +12,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<App />}>
       <Route index element={<Home />} />
       <Route path='nasa-projects' element={<NasaProjects />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Route>
   )
 )
