e3c3ec7e2b91ff259b8656183f30a3d6f6fceb06
sharmi4590 <bheemisettysharmila@gmail.com>
2024-06-01 08:37:12 +0530

    Responsive

--- src/components/Apod/Apod.jsx
+++ src/components/Apod/Apod.jsx
@@ -1,7 +1,8 @@
+// apod.jsx
+
 import React, { useState, useEffect } from 'react';
 import img from '../../assets/64e4e4aabd98a600197c0ca3.webp';
 import Styles from "./Apod.module.css"
-
 
 function Apod() {
 
@@ -12,24 +13,21 @@ function Apod() {
     date: "",
     copyright: "",
     media_type: "",
-  })
+  });
 
   async function fetchAPOD() {
-
     try {
-
-      let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${import.meta.env.VITE_API_KEY}`)
-
+      let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${import.meta.env.VITE_API_KEY}`);
       response = await response.json();
-
-      console.log(response);
-
-      setApodInfo((prev) => {
-        return { ...prev, url: response.url, explanation: response.explanation, title: response.title, date: response.date, copyright: response.copyright, media_type: response.media_type }
-      })
-
-      console.log(apodInfo);
-
+      setApodInfo((prev) => ({
+        ...prev,
+        url: response.url,
+        explanation: response.explanation,
+        title: response.title,
+        date: response.date,
+        copyright: response.copyright,
+        media_type: response.media_type,
+      }));
     } catch (error) {
       console.log(error.message);
     }
@@ -37,38 +35,30 @@ function Apod() {
 
   useEffect(() => {
     fetchAPOD();
-  }, [])
-
+  }, []);
 
   return (
-
     <>
-      <h1 align="center" className={Styles['section_title'] + " " + Styles['section_title_mobile']} id="apod">Astronomy Picture of The Day</h1 >
-
+      <h1 align="center" className={`${Styles.section_title} ${Styles.section_title_mobile}`} id="apod">Astronomy Picture of The Day</h1>
       <div className="container my-5">
-
-        <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg" id="class_removal">
-
+        <div className={`row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg ${Styles.responsive_layout}`} id="class_removal">
           <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
-
-            <h2 id="title" className="display-7 fw-bold lh-1 text-light">{apodInfo.title ? apodInfo.title : "APoD Title"}</h2>
-
+            <h2 id="title" className="display-7 fw-bold lh-1 text-light">{apodInfo.title || "APoD Title"}</h2>
             <br />
-
-            <p id="apod_info" className="lead">{apodInfo.explanation ? apodInfo.explanation : " NASA's Astronomy Picture of the Day (APoD) showcases captivating celestial images accompanied by brief explanations. Each day, NASA features astronomical wonders, ranging from stunning nebulae and galaxies to snapshots of our solar system, providing a daily dose of cosmic beauty and scientific insight. APoD serves as a valuable resource for both astronomy enthusiasts and those seeking to explore the wonders of the universe."}</p>
-
+            <p id="apod_info" className="lead">
+              {apodInfo.explanation || "NASA's Astronomy Picture of the Day (APoD) showcases captivating celestial images accompanied by brief explanations. Each day, NASA features astronomical wonders, ranging from stunning nebulae and galaxies to snapshots of our solar system, providing a daily dose of cosmic beauty and scientific insight. APoD serves as a valuable resource for both astronomy enthusiasts and those seeking to explore the wonders of the universe."}
+            </p>
           </div>
-
-          <div className={Styles["apod_img_container"]}>
-            <div className={Styles["img"]}>
+          <div className={`${Styles.apod_img_container} col-lg-5`}>
+            <div className={Styles.img}>
               {
-                apodInfo.media_type == "video" ?
-                  <iframe src={apodInfo.url ? apodInfo.url.replace("rel=1", "rel=0") + "&mute=1&color=white" : ""} id={Styles["apod_img"]} className={Styles["apod_img"]} alt="APOD" /> :
-                  <img src={apodInfo.url ? apodInfo.url : img} id={Styles["apod_img"]} className={Styles["apod_img"]} alt="APOD" />
+                apodInfo.media_type === "video" ? 
+                  <iframe src={apodInfo.url ? `${apodInfo.url.replace("rel=1", "rel=0")}&mute=1&color=white` : ""} id={Styles.apod_img} className={Styles.apod_img} alt="APOD" /> :
+                  <img src={apodInfo.url || img} id={Styles.apod_img} className={Styles.apod_img} alt="APOD" />
               }
             </div>
           </div>
-          {apodInfo.copyright ? <p>Took by {apodInfo.copyright} </p> : ""}
+          {apodInfo.copyright && <p>Took by {apodInfo.copyright}</p>}
         </div>
       </div>
     </>
