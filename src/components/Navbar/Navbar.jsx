6351dacf59e93c6adac2a9300f2a650d25072017
shreyash3087 <shreyash3087@gmail.com>
2024-06-05 19:35:50 +0530

    Fixed Navbar and Responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,108 +1,83 @@
-import React, { useEffect, useState } from 'react';
-import { NavLink, Link, useNavigate, useLocation } from 'react-router-dom'
-import logoWhite from '../../assets/logo_white.png';
-import menuFill from '../../assets/menu-fill.svg';
-import './navabar.css'
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import logoWhite from "../../assets/logo_white.png";
+import "./Navbar.css";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
 
-  const [isToggle, setIsToggle] = useState(false);
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  function toggleMenu() {
-    setIsToggle((prev) => !prev);
-  }
-
-  function handleMenuClose() {
-    setIsToggle(false);
-  }
-
-  function handleNavigation(hash) {
-    handleMenuClose();
-    navigate(`/#${hash}`);
-  }
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   useEffect(() => {
-    if (location.hash === '') {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    }else{
-      const element = document.getElementById(location.hash.substring(1));
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth' });
+    const handleResize = () => {
+      if (window.innerWidth > 1280 && isOpen) {
+        setIsOpen(false);
       }
-    }
-  }, [location]);
+    };
 
-  useEffect(() => {
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function () {
-      var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos < currentScrollPos) {
-        handleMenuClose()
-      } else {
-        handleMenuClose()
-      }
-      prevScrollpos = currentScrollPos;
-    }
-  }, []);
-
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isOpen]);
 
   return (
-    <nav className="navbar navbar-expand-lg bg-*">
-      <div className="container-fluid pt-2">
-        <a className="navbar-brand" onClick={() => {navigate('/#')}}>
-          <img src={logoWhite} alt="Bootstrap" width="370" />
+    <nav className="navbar">
+      <div className="logo">
+        <a href="#">
+          <img src={logoWhite} alt="Bootstrap" className="logo-img" />
         </a>
-
-        {/* burger menu for mobiles */}
-
-        <div className="sidebar-icon" id="sidebar-icon" onClick={toggleMenu}>
-          <img src={menuFill} alt="" className="sidebar-i" id="sidebar-i" />
-        </div>
-
-        <div className={"sidebar-menu" + (isToggle ? " active" : "")} id="sidebar-menu">
-          <div className="sidebar-links">
-            <ul>
-              <li className="sidebar-list-item rem" onClick={() => handleNavigation('')}><a className="rem-default">Home</a></li>
-              <li className="sidebar-list-item rem" onClick={() => handleNavigation('apod')}><a className="rem-default">APOD</a></li>
-              <li className="sidebar-list-item rem" onClick={handleMenuClose}><a className="rem-default" href="/mars-rover">MartianImagery</a></li>
-              <li className="sidebar-list-item rem" onClick={handleMenuClose}><NavLink className="rem-default" to="/nasa-projects">NASA's Projects</NavLink></li>
-              <li className="sidebar-list-item rem" onClick={handleMenuClose}><NavLink className="rem-default" to="/about">About us</NavLink></li>
-            </ul>
-            <a onClick={() => {navigate('/#contact')}} className="sidebar-contact rem-default rem">
-              <button className="btn btn-outline-light rounded-pill" type="submit">Contact Us</button>
+      </div>
+      <div className={`navigator ${isOpen ? "open" : ""}`}>
+        <ul className={`navbar-links ${isOpen ? "open" : ""}`}>
+          <li className="navbar-list-item">
+            <NavLink className="rem-default" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="navbar-list-item">
+            <a className="rem-default" href="#apod">
+              APOD
             </a>
-          </div>
-        </div>
-
-        {/* navbar for screens */}
-
-        <div className="collapse navbar-collapse pe-5" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item" onClick={() => handleNavigation('')}>
-              <Link className="nav-link text-light">Home</Link>
-            </li>
-            <li className="nav-item" onClick={() => handleNavigation('apod')}>
-              <a className="nav-link text-light">APoD</a>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link text-light" to="/mars-rover">MartianImagery</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link text-light" to="/nasa-projects">NASA's Projects</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link text-light" to="/about">About us</NavLink>
-            </li>
-            <li className="nav-item" onClick={() => {navigate('/#contact')}}>
-              <a>
-                <button className="btn btn-outline-light rounded-pill" type="submit">Contact Us</button>
-              </a>
-            </li>
-          </ul>
-        </div>
-
+          </li>
+          <li className="navbar-list-item">
+            <NavLink className="rem-default" to="/mars-rover">
+              MartianImagery
+            </NavLink>
+          </li>
+          <li className="navbar-list-item">
+            <NavLink className="rem-default" to="/nasa-projects">
+              NASA's Projects
+            </NavLink>
+          </li>
+          <li className="navbar-list-item">
+            <NavLink className="rem-default" to="/about">
+              About us
+            </NavLink>
+          </li>
+          <li className="navbar-list-item">
+            <a href="#contact" className="sidebar-contact rem-default rem">
+              <button
+                className="btn btn-outline-light rounded-pill"
+                type="submit"
+              >
+                Contact Us
+              </button>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div
+        className="hamburger"
+        id="hamburger"
+        onClick={toggleMenu}
+        aria-label="Menu"
+      >
+        <div className={`bar ${isOpen ? "bar1-open" : ""}`}></div>
+        <div className={`bar ${isOpen ? "bar2-open" : ""}`}></div>
+        <div className={`bar ${isOpen ? "bar3-open" : ""}`}></div>
       </div>
     </nav>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import App from './App.jsx'
 import Home from "./pages/Home/Home.jsx"
 import NasaProjects from "./pages/NasaProjects/NasaProjects.jsx"
 import NotFoundPage from "./pages/404 Page/NotfoundPage.jsx"
-import './index.css'
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom"
 import About from "./pages/About/About.jsx"
 import MarsRover from './pages/MarsRover/MarsRover.jsx'
 
