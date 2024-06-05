c6b44738ee991565029a9965fbe98a5572158f42
Donald Reddy Indelu <donaldreddy2712@gmail.com>
2024-06-05 19:36:47 +0530

    add 3D models

--- src/pages/MarsRover/MarsRover.jsx
+++ src/pages/MarsRover/MarsRover.jsx
@@ -3,6 +3,10 @@ import img from "../../assets/mars.png";
 import DisplayDetails from "../../components/DisplayDetails/DisplayDetails.jsx";
 import { toastify } from "../../components/Toast/Toast.jsx";
 import ReadableStreamDecoder from './../../utils/ReadableStreamDecoder';
+import Styles from "./MarsRover.module.css"
+import Curiosity from "../../components/3D_Rovers/Curiosity.jsx"
+import Perseverance from "../../components/3D_Rovers/Perseverance.jsx"
+import SpritOpp from "../../components/3D_Rovers/SpritOpp.jsx";
 
 function MarsRover() {
 
@@ -85,68 +89,72 @@ function MarsRover() {
 
 
     return (
-        <>
-            <h1 align="center" className="section_title section_title_mobile" id="mars">Mars Rover Imagery</h1>
+        <div id={Styles['container']}>
+            <h1 className="section_title section_title_mobile" id="mars">Mars Rover Imagery</h1>
             <br />
 
-                <div className="rover_container">
-                    <div className="rover_section_img">
-                        <img src={img} className="mars_illus" alt="Mars" />
-                    </div>
-                    <div className="rover_section_text">
-                        <p className="rover_section_para">
-                            Welcome to an extraordinary adventure into the Martian landscape,
-                            where we invite you to explore the wonders of the Red Planet through
-                            the lens of various rovers that have traversed its surface. Our
-                            journey takes us deep into the photographic archives, a treasure
-                            trove of captivating images captured by these robotic explorers.
-                            Each rover, armed with sophisticated cameras, has documented the
-                            Martian terrain, unveiling its mysteries one snapshot at a time.
-                            This repository is a testament to the marvels of technology and
-                            the spirit of scientific curiosity that drives
-                            humanity to push the boundaries of exploration. But how do you, the
-                            explorer, access this vast collection? Fear not, for we present a
-                            user-friendly approach that allows you to effortlessly navigate
-                            through the visual wonders of Mars. By simply entering the Earth
-                            date corresponding to a specific photo, you unlock a portal to the
-                            Red Planet's past, witnessing moments frozen in time by the lenses
-                            of our robotic companions. The convenience of this method provides
-                            unparalleled flexibility, enabling you to tailor your exploration
-                            based on your interests. Join us in unlocking the mysteries of
-                            Mars as we navigate through the cutting-edge technology and
-                            unparalleled exploration that has brought us face to face with the
-                            enigmatic Red Planet.
-                        </p>
-                    </div>
+            <section id={Styles['section-1']}>
+                <div >
+                    <img src={img} alt="Mars" />
                 </div>
+                <p >
+                    Welcome to an extraordinary adventure into the Martian landscape,
+                    where we invite you to explore the wonders of the Red Planet through
+                    the lens of various rovers that have traversed its surface. Our
+                    journey takes us deep into the photographic archives, a treasure
+                    trove of captivating images captured by these robotic explorers.
+                    Each rover, armed with sophisticated cameras, has documented the
+                    Martian terrain, unveiling its mysteries one snapshot at a time.
+                    This repository is a testament to the marvels of technology and
+                    the spirit of scientific curiosity that drives
+                    humanity to push the boundaries of exploration. But how do you, the
+                    explorer, access this vast collection? Fear not, for we present a
+                    user-friendly approach that allows you to effortlessly navigate
+                    through the visual wonders of Mars. By simply entering the Earth
+                    date corresponding to a specific photo, you unlock a portal to the
+                    Red Planet's past, witnessing moments frozen in time by the lenses
+                    of our robotic companions.
+                </p>
+            </section>
 
-            <div className="ip">
-                <div className="input-group mb-4 input_container">
-                    <input
-                        className="date_input form-control"
-                        type="date"
-                        name="date"
-                        aria-label="Recipient's username"
-                        aria-describedby="button-addon2"
-                        min="2015-06-03"
-                        max={maxDate}
-                        value={userDate}
-                        onChange={(e) => setUserDate((prev) => e.target.value)}
-                    />
-                    <button
-                        className="btn btn-primary mars_button"
-                        type="button"
-                        id="button-addon2"
-                        onClick={displayRover}
-                    >
-                        Enter
-                    </button>
+
+
+            <section id={Styles['section-2']}>
+                <h1>Rovers on Mars</h1>
+                <div>
+                    <div className={Styles['rover-wrapper']}><Curiosity /></div>
+                    <div className={Styles['rover-wrapper']}><Perseverance /></div>
+                    <div className={Styles['rover-wrapper']}><SpritOpp /></div>
                 </div>
+            </section>
+
+
+
+            <div id={Styles['calender']}>
+                <input
+                    className="date_input form-control"
+                    type="date"
+                    name="date"
+                    aria-label="Recipient's username"
+                    aria-describedby="button-addon2"
+                    min="2015-06-03"
+                    max={maxDate}
+                    value={userDate}
+                    onChange={(e) => setUserDate((prev) => e.target.value)}
+                />
+                <button
+                    className="btn btn-primary mars_button"
+                    type="button"
+                    id="button-addon2"
+                    onClick={displayRover}
+                >
+                    Enter
+                </button>
             </div>
 
             {showImages && (<DisplayDetails roverInfo={roverInfo} fun={setShowImages} />)}
 
-        </>
+        </div>
     );
 }
 
