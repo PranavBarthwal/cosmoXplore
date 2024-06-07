00a19be078b4a3327c0de4157ce295142c55ca7c
Aishwarya <aishuchandrasekaran6@gmail.com>
2024-06-07 14:49:42 +0530

    chatbot

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,10 @@ import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } 
 import './index.css'
 import App from './App.jsx'
 import Home from "./pages/Home/Home.jsx"
-import NasaProjects from "./pages/NasaProjects/NasaProjects.jsx"
-import NotFoundPage from "./pages/404 Page/NotfoundPage.jsx"
-import './index.css'
-import About from "./pages/About/About.jsx"
 import MarsRover from './pages/MarsRover/MarsRover.jsx'
+import NasaProjects from "./pages/NasaProjects/NasaProjects.jsx"
+import About from "./pages/About/About.jsx"
+import NotFoundPage from "./pages/404 Page/NotfoundPage.jsx"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
