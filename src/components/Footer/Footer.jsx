3e5f0975d7899567334687fa86cfc5e9b329927d
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2024-06-18 11:36:06 +0530

    Added social media links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import img from '../../assets/logo_white.png';
 
-
 function Footer() {
   const currentYear = new Date().getFullYear();
   return (
@@ -10,7 +9,21 @@ function Footer() {
         <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
           <img src={img} width="150" alt="Logo" />
         </a>
-        <p className="col-md-4 mb-0 text-light">©️ {currentYear} CosmoXplore India, Inc. All Rights Reserved</p>
+        <p className="col-md-4 mb-0 text-light text-center">©️ {currentYear} CosmoXplore India, Inc. All Rights Reserved</p>
+        <div className="col-md-4 d-flex justify-content-center">
+          <a href="https://www.facebook.com" className="text-light me-4">
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://www.twitter.com" className="text-light me-4">
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="https://www.instagram.com" className="text-light me-4">
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://www.linkedin.com" className="text-light">
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+        </div>
       </footer>
     </div>
   );
