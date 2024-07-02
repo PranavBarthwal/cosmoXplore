aa2c7b85c5eed065eee6458e4f905cd67c12df50
Gaurav-Kushwaha-1225 <gaurav.family04@gmail.com>
2024-07-02 20:39:22 +0530

    All required changes working properly except scroll bar

--- src/components/ScrollToTop/ScrollToTop.jsx
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -2,9 +2,20 @@ import React, { useState } from 'react'
 import Styles from './ScrollToTop.module.css'
 import { TbArrowBigUpLineFilled } from "react-icons/tb";
 
+function MarsRoverActive() {
+    var path = window.location.pathname;
+
+    if (path === '/marsrover') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 function ScrollToTop() {
 
-    const [isHover,setIsHover]=useState(false);
+    const [isHover, setIsHover] = useState(false);
 
     function handleClick(e) {
         window.scrollTo(
@@ -15,9 +26,20 @@ function ScrollToTop() {
         )
     }
 
+    let currentStyleBtn = "btn_others";
+    let currentStyleScrollBtn = "scroll-btn_others";
+
+    if (MarsRoverActive()) {
+        currentStyleBtn = "btn_marsrover";
+        currentStyleScrollBtn = "scroll-btn_marsrover";
+    } else {
+        currentStyleBtn = "btn_others";
+        currentStyleScrollBtn = "scroll-btn_others";
+    }
+
     return (
-        <div className={Styles['btn']} onMouseEnter={()=>setIsHover(true)} onMouseLeave={()=>setIsHover(false)}>
-            <button className={Styles['scroll-btn']} onClick={handleClick}><TbArrowBigUpLineFilled size={25} color={isHover?'white':'#720026'} /></button>
+        <div className={Styles[currentStyleBtn]} onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
+            <button className={Styles[currentStyleScrollBtn]} onClick={handleClick}><TbArrowBigUpLineFilled size={25} color={isHover ? 'white' : MarsRoverActive() ? '#720026' : '#8400ff'} /></button>
         </div>
     )
 }
--- src/pages/MarsRover/MarsRover.jsx
+++ src/pages/MarsRover/MarsRover.jsx
@@ -90,6 +90,11 @@ function MarsRover() {
         }
     }
 
+    if (window.location.pathname === '/marsrover') {
+        document.body.style.backgroundImage = "linear-gradient(to left, rgba(0, 0, 0, 0.9), rgba(27, 0, 0, 1))";
+    } else {
+        document.body.style.backgroundImage = "linear-gradient(to left, rgb(10, 0, 73), rgb(0, 0, 0))";
+    }
 
     return (
         <div id={Styles['container']}>
